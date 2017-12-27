0ff5f9e148baba8c7ec30b65e0454119437923e2
fvargasestrada <paco@sauz.es>
2017-12-27 13:39:14 +0100

    probando con deviceready

--- platforms/android/assets/www/js/monitor.js
+++ platforms/android/assets/www/js/monitor.js
@@ -2,12 +2,26 @@ function display($str){
     $("#display").append($str + "<br />");
 }
 
-$(function(){
-    console.log("probando funciones");
-    display("Antes de las funciones");
-    cordova.plugins.simplexpbeacon.initialiseBluetooth(
+function lanzaMonitor(){
+    console.log(cordova);
+    console.log(cordova.plugins);
+    display("device ready");
+    cordova.plugins.simplexpbeacon.startMonitoring(
         function(data) {
             display("dentro de la funcion");
         }
     );
-});
+}
+
+document.addEventListener("deviceready", lanzaMonitor, false);
+
+// $(function(){
+//     console.log("probando funciones");
+//     display("Antes de las funciones");
+//     lanzaMonitor();
+//     cordova.plugins.simplexpbeacon.initialiseBluetooth(
+//         function(data) {
+//             display("dentro de la funcion");
+//         }
+//     );
+// });
