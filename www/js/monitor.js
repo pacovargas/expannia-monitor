c90581906e47504fa1ea5688ede6c47f0d73bbb6
fvargasestrada <paco@sauz.es>
2017-12-28 20:38:04 +0100

    probando timeout

--- www/js/monitor.js
+++ www/js/monitor.js
@@ -71,25 +71,28 @@ function lanzaMonitor(){
         }
     );
 
-    cordova.plugins.simplexpbeacon.startMonitoring(
-        function(data) {
-            var json = JSON.parse(data);
-            // display("start status: " + json.status);
-            // display("start event: " + json.event);
-            // display("start desc: " + json.desc);
-            if(json.status == "OK" && json.event == "IBEACON"){
-                if(cont < 1000)
-                    cont = cont + 1;
-                else
-                    cont = 0;
-                var distancia = parseInt(json.data["rssi"]) / parseInt(json.data["txpower"]);
-                display(cont + " - BEACON: " + json.data["uuid"] + "-" + json.data["major"] + "-" + json.data["minor"] + "(rssi: " + json.data["rssi"] + ") (Distiancia: " + distancia);
-                if(name != ""){
-                    updateVehicleLocation(json.data["uuid"] + "-" + json.data["major"] + "-" + json.data["minor"], name, distancia);
+    setTimeout(function(){
+        cordova.plugins.simplexpbeacon.startMonitoring(
+            function(data) {
+                var json = JSON.parse(data);
+                // display("start status: " + json.status);
+                // display("start event: " + json.event);
+                // display("start desc: " + json.desc);
+                if(json.status == "OK" && json.event == "IBEACON"){
+                    if(cont < 1000)
+                        cont = cont + 1;
+                    else
+                        cont = 0;
+                    var distancia = parseInt(json.data["rssi"]) / parseInt(json.data["txpower"]);
+                    display(cont + " - BEACON: " + json.data["uuid"] + "-" + json.data["major"] + "-" + json.data["minor"] + "(rssi: " + json.data["rssi"] + ") (Distiancia: " + distancia);
+                    if(name != ""){
+                        updateVehicleLocation(json.data["uuid"] + "-" + json.data["major"] + "-" + json.data["minor"], name, distancia);
+                    }
                 }
             }
-        }
-    );
+        );
+    }, 100);
+    
 }
 
 document.addEventListener("deviceready", lanzaMonitor, false);
