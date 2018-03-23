2c1e36fc53848751773310869ca389aa2a4234e8
fvargasestrada <paco@sauz.es>
2018-03-23 16:55:13 +0100

    background mode

--- platforms/android/assets/www/js/monitor.js
+++ platforms/android/assets/www/js/monitor.js
@@ -101,33 +101,9 @@ document.addEventListener("deviceready", lanzaMonitor, false);
 cordova.plugins.backgroundMode.onactivate = function() {
     //La función
     //Añadir un setTimeout(funcion, 10000); para hacer que la función se ejecute cada x segundos
-    navigator.geolocation.watchPosition(
-        function(position){
-            display("New position: " + position.coords.latitude + ":" + position.coords.longitude);
-            updatePosition(position.coords.latitude, position.coords.longitude);
-        }
-    );
 
     setTimeout(function(){
-        cordova.plugins.simplexpbeacon.startMonitoring(
-            function(data) {
-                var json = JSON.parse(data);
-                // display("start status: " + json.status);
-                // display("start event: " + json.event);
-                // display("start desc: " + json.desc);
-                if(json.status == "OK" && json.event == "IBEACON"){
-                    if(cont < 1000)
-                        cont = cont + 1;
-                    else
-                        cont = 0;
-                    var distancia = parseInt(json.data["rssi"]) / parseInt(json.data["txpower"]);
-                    display(cont + " - BEACON: " + json.data["uuid"] + "-" + json.data["major"] + "-" + json.data["minor"] + "(rssi: " + json.data["rssi"] + ") (Distiancia: " + distancia);
-                    if(name != ""){
-                        updateVehicleLocation(json.data["uuid"] + "-" + json.data["major"] + "-" + json.data["minor"], name, distancia);
-                    }
-                }
-            }
-        );
+        display("en background");
     }, 1);
 };
 
